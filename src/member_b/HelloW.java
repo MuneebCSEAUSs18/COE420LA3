fcb678e362d526a50882d6e788cf415dcc06ea4f
Syed Mashadi <b00053832@aus.edu>
2018-02-19 18:59:33 +0400

    Feature B added

---
+++ src/member_b/HelloW.java
@@ -0,0 +1,14 @@
+package member_b;
+
+public class HelloW {
+
+	public static void main(String[] args) {
+		System.out.println("Hi !");
+
+	}
+	public void addFeatureB(){
+		System.out.println("Hi I am member B!");
+		
+	}
+
+}
