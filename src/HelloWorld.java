26ab3146e007db57f961fb8deb2cb9c166ba669d
jerry00jose <36403796+jerry00jose@users.noreply.github.com>
2018-02-19 19:28:24 +0400

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello World, I have changed the comma now");
     }
 	public void addFeatureB(){
 		System.out.println("Hi I am member B!");
